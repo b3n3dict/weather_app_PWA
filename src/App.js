33acef7cb98e1972a8caea2255a6c4f517cf5ede
Benedict Xavier <benedict.xavier.e@gmail.com>
2020-11-18 14:14:44 +0530

    App.css added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
-
-
+import {fetchWeather} from './api/fetchWeather'
+import './App.css'
 const App =()=>{
 
     return(
--- src/api/fetchWeather.js
+++ src/api/fetchWeather.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const URL = 'https://api.openweathermap.org/data/2.5/weather'
 const API_KEY ='f33a484cf794d08d0148764789aaba32'
 
-export const fetchWheather = async (cityName)  =>{
+export const fetchWeather = async (cityName)  =>{
    const {data} = await axios.get(URL,{
        params:{
            q: cityName,
