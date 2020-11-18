e33f0dea41caab6c88fb5e1b2ef6c9f5b8b21281
Benedict Xavier <benedict.xavier.e@gmail.com>
2020-11-18 15:23:56 +0530

    weather detail display card added

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import {fetchWeather} from './api/fetchWeather'
 import './App.css'
 const App =()=>{
       const [cityName, setCityName] = useState('')
+      const [weather,setWeather] = useState({})
       const search = async(e)=>{
            if(e.key === 'Enter'){
             const data = await fetchWeather(cityName)
-            console.log(data)
+            setWeather(data)
+            setCityName('')
            }
       }
     return(
@@ -21,7 +23,24 @@ const App =()=>{
              }}
             onKeyPress={search}
          />
-
+               {weather.main && (
+                   <div className="city"> 
+                          <h2 className="city-name">
+                              <span>{weather.name}</span>
+                              <sup>{weather.sys.country}</sup>
+                          </h2>
+                          <div className="city-temp">
+                            {Math.round(weather.main.temp)}
+                            <sup>&deg;C</sup>
+                            <p>Feels Like:{Math.round(weather.main.feels_like)}
+                            <sup>&deg;C</sup></p>
+                          </div>
+                          <div className="info">
+                          <img className="city-icon" src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description} />
+                          <p>{weather.weather[0].description}</p>
+                          </div>
+                    </div>
+               )}
         </div>
     )
 }
