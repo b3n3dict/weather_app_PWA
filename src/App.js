4b6f6b8aaec2d7077bce661d9e145895f041e3f4
Benedict Xavier <benedict.xavier.e@gmail.com>
2020-11-18 14:30:18 +0530

    input filed created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from 'react'
+import React,{useState} from 'react'
 import {fetchWeather} from './api/fetchWeather'
 import './App.css'
 const App =()=>{
-
+      const [cityName, setCityName] = useState('')
     return(
-        <h1>App</h1>
+        <div className="main-container">
+         <input 
+             type="text"
+             className="search"
+             placeholder="Search..."
+             value={cityName}
+             onChange={(e)=>{
+               setCityName(e.target.value)
+             }}
+         />
+        </div>
     )
 }
 
